c72e99edd531f7c617be4453ab6b8c166402377b
oancea-ilie <oancea.ilie@outlook.com>
2022-04-06 20:37:19 +0300

    ceva

--- server.js
+++ server.js
@@ -27,16 +27,11 @@ app.get('/api', (req, res) => {
 });
 
 const __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client/build')))
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    )
-}else{
-    app.get('/', (req, res) => {
-      res.send('API is running....')
-    })
-}
+
+app.use(express.static(path.join(__dirname, '/client/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+)
 
 app.use('/api/products', productRoutes);
 app.use('/api/customers', customerRoutes);
