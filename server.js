145063c6f06b342a493dc1e8ea09980c5b0748cb
oancea-ilie <oancea.ilie@outlook.com>
2022-04-06 20:14:47 +0300

    first commit

--- server.js
+++ server.js
@@ -20,14 +20,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors());
 
-const __dirname=path.resolve();
-
 app.get('/api', (req, res) => {
     res.json({
         message: 'Welcome to the REST API project!',
     });
 });
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req, res) =>
